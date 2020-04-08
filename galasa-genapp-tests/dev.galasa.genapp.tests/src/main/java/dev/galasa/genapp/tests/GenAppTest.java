1c2d4040a054f31cd79e90a0aa745ea9a65b7259
Richard Somers <rsomers1@sheffield.ac.uk>
2020-04-08 15:35:27 +0100

    GenApp IVT

---
+++ galasa-genapp-tests/dev.galasa.genapp.tests/src/main/java/dev/galasa/genapp/tests/GenAppTest.java
@@ -0,0 +1,51 @@
+package dev.galasa.genapp.tests;
+
+import dev.galasa.ICredentialsUsernamePassword;
+import dev.galasa.Test;
+import dev.galasa.core.manager.CoreManager;
+import dev.galasa.core.manager.CoreManagerException;
+import dev.galasa.core.manager.ICoreManager;
+import dev.galasa.zos.IZosImage;
+import dev.galasa.zos.ZosImage;
+import dev.galasa.zos3270.ITerminal;
+import dev.galasa.zos3270.Zos3270Exception;
+import dev.galasa.zos3270.Zos3270Terminal;
+
+@Test
+public class GenAppTest {
+
+    @CoreManager
+    public ICoreManager coreManager;
+
+    @ZosImage(imageTag = "GENAPP")
+    public IZosImage image;
+
+    @Zos3270Terminal(imageTag = "GENAPP")
+    public ITerminal terminal;
+
+    final String APPLID = "CICPY01D";
+
+    @Test
+    public void login() throws InterruptedException, CoreManagerException, Zos3270Exception {
+        terminal.waitForKeyboard().type("logon applid(" + APPLID + ")")
+                .enter().waitForTextInField("Userid")
+                .pf3().waitForTextInField("Sign-on is terminated").clear().waitForKeyboard()
+                .type("cesl").enter().waitForTextInField("Userid");
+
+        ICredentialsUsernamePassword creds = (ICredentialsUsernamePassword) coreManager.getCredentials("RICH98");
+        coreManager.registerConfidentialText(creds.getPassword(), "Rich98 password");
+
+        terminal.positionCursorToFieldContaining("Userid").tab()
+                .type(creds.getUsername())
+                .positionCursorToFieldContaining("Password").tab()
+                .type(creds.getPassword())
+                .enter().waitForKeyboard()
+                .clear().waitForKeyboard()
+
+                .type("ssc1").enter().waitForKeyboard()
+                .positionCursorToFieldContaining("Cust Number").tab()
+                .type("0000000013")
+                .positionCursorToFieldContaining("Select Option").tab()
+                .type("1").enter().waitForKeyboard();
+    }
+}
