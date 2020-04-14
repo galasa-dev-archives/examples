70f428d8217f7e63537e46158b023a467b5f6c3b
LukasMarivoetIBM <lukas.marivoet@ibm.com>
2020-04-14 13:13:48 +0200

    Commenting Basictest and clean-up

--- galasa-genapp-tests/dev.galasa.genapp.tests/src/main/java/dev/galasa/genapp/tests/BasicTest.java
+++ galasa-genapp-tests/dev.galasa.genapp.tests/src/main/java/dev/galasa/genapp/tests/BasicTest.java
@@ -47,15 +47,21 @@ public class BasicTest {
     @HttpClient
     public IHttpClient client;
 
+    //ApplID of the CICS-region that contains the GenApp installation 
     final String APPLID = "CICPY01D";
 
     @Test
     public void test() throws CoreManagerException, InterruptedException, Zos3270Exception,
             TestBundleResourceException, JsonSyntaxException, IOException, HttpClientException, URISyntaxException {
+        // Coupling the URI of GenApp to this instance of the Http-client
         client.setURI(new URI("http://"+ image.getIpHost().getHostname() +":23571"));
         
         HashMap<String, Object> addParameters = new HashMap<String, Object>();
+        HashMap<String, Object> InquireParameters = new HashMap<String, Object>();
         String runId = coreManager.getRunName();
+        String defaultId = "0000000000";
+
+        //Adding a customer using the exposed http-ports of the CICS-region that has GenApp running
         addParameters.put("ID", runId);
         InputStream addIs = bundleResources.retrieveSkeletonFile("resources/skeletons/customerAdd.skel", addParameters);
         JsonObject customerAddJson = new Gson().fromJson(bundleResources.streamAsString(addIs), JsonObject.class);
@@ -64,7 +70,7 @@ public class BasicTest {
                                         .get("ca").getAsJsonObject()
                                         .get("ca_customer_num").getAsString();
         
-        HashMap<String, Object> InquireParameters = new HashMap<String, Object>();
+        //Inquiring about the added customer using the exposed http-ports of the CICS-region that has GenApp running
         InquireParameters.put("NUM", customerId);
         InputStream inquireIs = bundleResources.retrieveSkeletonFile("resources/skeletons/customerInquire.skel", InquireParameters);
         JsonObject customerInquireJson = new Gson().fromJson(bundleResources.streamAsString(inquireIs), JsonObject.class);
@@ -72,12 +78,12 @@ public class BasicTest {
         JsonObject ca = inquireResponseBody.get("LGICUS01OperationResponse").getAsJsonObject()
                                    .get("ca").getAsJsonObject();
 
+        //Ensuring that the http-response actually contains user as requested
         assertThat(ca.get("ca_first_name").getAsString()).isEqualTo("Test" + runId);
         assertThat(ca.get("ca_last_name").getAsString()).isEqualTo("Case" + runId);
 
+        //Ensuring that the added customer is also available through 3270-terminal
         loginToSCC1();
-
-        String defaultId = "0000000000";
         terminal.positionCursorToFieldContaining("Cust Number").tab()
                 .type(defaultId.substring(0,defaultId.length()-customerId.length()) + customerId).enter().waitForKeyboard()
                 .positionCursorToFieldContaining("Select Option").tab()
@@ -88,13 +94,15 @@ public class BasicTest {
     }
 
     private void loginToSCC1() throws InterruptedException, CoreManagerException, Zos3270Exception {
+        //Retrieving the credentials that have the correct RACF-premissions to enter GenApp
+        ICredentialsUsernamePassword creds = (ICredentialsUsernamePassword) coreManager.getCredentials("GENAPP");
+        coreManager.registerConfidentialText(creds.getPassword(), creds.getUsername() + " password");
+        //Logging into the CICS-region that has GenApp running on it
         terminal.waitForKeyboard().type("logon applid(" + APPLID + ")")
                 .enter().waitForTextInField("Userid")
                 .pf3().waitForTextInField("Sign-on is terminated").clear().waitForKeyboard()
-                .type("cesl").enter().waitForTextInField("Userid");
-        ICredentialsUsernamePassword creds = (ICredentialsUsernamePassword) coreManager.getCredentials("GENAPP");
-        coreManager.registerConfidentialText(creds.getPassword(), creds.getUsername() + " password");
-        terminal.positionCursorToFieldContaining("Userid").tab()
+                .type("cesl").enter().waitForTextInField("Userid")
+                .positionCursorToFieldContaining("Userid").tab()
                 .type(creds.getUsername())
                 .positionCursorToFieldContaining("Password").tab()
                 .type(creds.getPassword())
