256a7d285ffe1bf15fe49ac3a2fe37504986c721
Richard Somers <rsomers1@sheffield.ac.uk>
2020-04-16 09:57:42 +0100

    Removed core manager + comments

--- galasa-genapp-tests/dev.galasa.genapp.tests/src/main/java/dev/galasa/genapp/tests/ProvisionedTest.java
+++ galasa-genapp-tests/dev.galasa.genapp.tests/src/main/java/dev/galasa/genapp/tests/ProvisionedTest.java
@@ -53,58 +53,61 @@ public class ProvisionedTest {
     @StoredArtifactRoot
     public Path artifactRoot;
 
-    @CoreManager
-    public ICoreManager coreManager;
-
     @Test
     public void prosivionedAccountTest() throws GenAppManagerException, URISyntaxException, TestBundleResourceException,
             JsonSyntaxException, IOException, HttpClientException {
+        //Generate a random string and update the first name of the customer using a 3270 terminal
         String provisionedName = genApp.provisionCustomerName();
-
         customer.updateFirstName(provisionedName);
 
+        //Set up the customer inquire Json request
         HashMap<String, Object> inquireParameters = new HashMap<String, Object>();
         inquireParameters.put("NUM", customer.getCustomerNumber());
         InputStream inquireIs = resources.retrieveSkeletonFile("resources/skeletons/customerInquire.skel", inquireParameters);
         String jsonRequest = resources.streamAsString(inquireIs);
 
+        //Store the request to the RAS
         storeOutput("apicall", "inquireRequest.txt", jsonRequest);
 
+        //Invoke the Json request
         JsonObject customerInquireJson = new Gson().fromJson(jsonRequest, JsonObject.class);
-
         client.setURI(new URI(genApp.getAddress()));
         JsonObject inquireResponseBody = client.postJson(genApp.getInquireCustomerPath(), customerInquireJson).getContent();
 
+        //Store the Json service response
         storeOutput("apicall", "inquireResponse.txt", new Gson().toJson(inquireResponseBody));
 
+        //Ensure that the first name in the Json response is the updated value
         String webServiceFirstName = extractFromInquireJson(inquireResponseBody, "ca_first_name");
-
         assertThat(webServiceFirstName).isEqualTo(customer.getFirstName());
     }
 
     @Test
     public void addWebserviceAccount() throws TestBundleResourceException, IOException, HttpClientException,
             URISyntaxException, GenAppManagerException {
+        //Set up an add customer Json request with a random string generated for the name
         String provisionedName = genApp.provisionCustomerName();
-
         HashMap<String, Object> addParameters = new HashMap<String, Object>();
         addParameters.put("ID", provisionedName);
         InputStream addIs = resources.retrieveSkeletonFile("resources/skeletons/customerAdd.skel", addParameters);
         String jsonRequest = resources.streamAsString(addIs);
 
+        //Store the Json request in the RAS
         storeOutput("apicall", "addRequest.txt", jsonRequest);
 
+        //Invoke the Json request
         JsonObject customerAddJson = new Gson().fromJson(jsonRequest, JsonObject.class);
-
         client.setURI(new URI(genApp.getAddress()));
         JsonObject addResponseBody = client.postJson(genApp.getAddCustomerPath(), customerAddJson).getContent();
 
+        //Store the Json response in the RAS
         storeOutput("apicall", "addResponse.txt", new Gson().toJson(addResponseBody));
 
+        //Retrieve the created customer data using a 3270 terminal
         int customerNumber = extractFromAddJson(addResponseBody);
-
         ICustomer customer = genApp.inquireCustomer(customerNumber);
 
+        //Assert that the customer was created correctly
         assertThat(customer).isNotNull();
         assertThat(customer.getFirstName()).isEqualTo(provisionedName);
         assertThat(customer.getLastName()).isEqualTo(provisionedName);
